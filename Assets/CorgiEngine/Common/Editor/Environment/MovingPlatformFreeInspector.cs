8db42e5214ae1d4e4e40e0ad070db2bb8c754ec1
TokeninKuntarto <tokenin.id@gmail.com>
2020-08-29 16:02:50 +0800

    First Commit
    First Commit for the Si Bejo

---
+++ Assets/CorgiEngine/Common/Editor/Environment/MovingPlatformFreeInspector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MoreMountains.CorgiEngine
+{
+
+	[CustomEditor (typeof(MovingPlatformFree))]
+	[CanEditMultipleObjects]
+
+	/// <summary>
+	/// Adds custom labels to the CorgiController inspector
+	/// </summary>
+
+	public class MovingPlatformFreeInspector : Editor 
+	{
+        protected MovingPlatformFree _movingPlatformFree;
+        
+        protected SerializedProperty _debugCurrentSpeed;
+
+        public override bool RequiresConstantRepaint()
+        {
+            return true;
+        }
+
+        protected virtual void OnEnable()
+        {
+            _movingPlatformFree = target as MovingPlatformFree;
+            _debugCurrentSpeed = serializedObject.FindProperty("DebugCurrentSpeed");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawProperties();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected virtual void DrawProperties()
+        {
+            EditorGUILayout.PropertyField(_debugCurrentSpeed);
+        }
+    }
+}
---
+++ Assets/CorgiEngine/Common/Scripts/Environment/MovingPlatformFree.cs
@@ -0,0 +1,31 @@
+﻿using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreMountains.CorgiEngine
+{
+    public class MovingPlatformFree : MMPathMovement
+    {
+        /// a debug display of this platform's current speed
+        [MMReadOnly]
+        [Tooltip("a debug display of this platform's current speed")]
+        public Vector3 DebugCurrentSpeed; 
+
+        protected Vector3 _newSpeed;
+        protected Vector3 _positionLastFrame;
+
+        protected override void Update()
+        {
+            _newSpeed = (this.transform.position - _positionLastFrame) / Time.deltaTime;
+            CurrentSpeed = _newSpeed;
+            DebugCurrentSpeed = CurrentSpeed;
+        }
+
+        protected override void LateUpdate()
+        {
+            _positionLastFrame = this.transform.position;
+
+        }
+    }
+}
